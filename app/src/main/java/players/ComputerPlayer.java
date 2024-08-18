599204cc33cde6a20159b834afee3009c586cab7
Angel <gabrielvargas493@gmail.com>
2024-08-18 15:30:03 -0600

    Se hacen cambios en ComputerPlayer

--- app/src/main/java/players/ComputerPlayer.java
+++ app/src/main/java/players/ComputerPlayer.java
@@ -6,29 +6,35 @@ import gamedifficulties.HardDifficulty;
 import gamedifficulties.MediumDifficulty;
 
 public class ComputerPlayer implements Player {
-    private final Player difficulty;
+    private Player difficulty;
     private final char symbol;
+    private final String difficultyLevel;
+    private final int maxRow;
+    private final int maxColumn;
 
     public ComputerPlayer(final char symbol, final String difficultyLevel, final int maxRow, final int maxColumn) {
         this.symbol = symbol;
-
-        switch (difficultyLevel) {
-            case "f":
-                this.difficulty = new EasyDifficulty(maxRow, maxColumn);
-                break;
-            case "m":
-                this.difficulty = new MediumDifficulty(maxRow, maxColumn);
-                break;
-            case "d":
-                this.difficulty = new HardDifficulty(maxRow, maxColumn);
-                break;
-            default:
-                throw new IllegalArgumentException("Dificultad inválida: " + difficultyLevel);
-        }
+        this.difficultyLevel = difficultyLevel;
+        this.maxRow = maxRow;
+        this.maxColumn = maxColumn;
     }
 
     @Override
     public int[] makeMove(GameBoard gameBoard) {
-        return new int[0];
+        Player difficulty;
+        switch (difficultyLevel) {
+            case "f":
+                difficulty = new EasyDifficulty(maxRow, maxColumn);
+                break;
+            case "m":
+                difficulty = new MediumDifficulty(maxRow, maxColumn);
+                break;
+            case "d":
+                difficulty = new HardDifficulty(maxRow, maxColumn);
+                break;
+            default:
+                throw new IllegalArgumentException("Dificultad inválida: " + difficultyLevel);
+        }
+        return difficulty.makeMove(gameBoard);
     }
 }
